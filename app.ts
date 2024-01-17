de72324b511b23596f2ccf97c756adbe620605b5
m6tm <maboadaniel.55@gmail.com>
2024-01-17 08:00:37 +0100

    Reformattage du projet

--- app.ts
+++ app.ts
@@ -1,23 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,4 +39,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app
---
+++ src/app.ts
@@ -0,0 +1,79 @@
+import path from 'path';
+import app from '../app';
+import debug from "debug";
+import http from "http";
+
+let package_json = require(path.join(process.cwd(), "package.json"));
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const local_hosts = package_json.app_env != 'local'? [] : [
+  "http://localhost:3000",
+  "http://localhost:8000",
+]
+
+var port = normalizePort(process.env.PORT || 3500) as number,
+  origins = [
+    ...local_hosts,
+  ];
+app
+        .set('port', port)
+        .listen(port, () => {
+                console.log(
+                    '%s App is running at http://localhost:%d in %s mode',
+                    '>>>',
+                    port,
+                    app.get('env')
+                );
+            })
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val: any) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: any) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
